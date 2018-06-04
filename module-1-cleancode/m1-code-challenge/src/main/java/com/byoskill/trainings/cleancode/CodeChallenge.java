ef3e55bdc0eeaf9e61d3760349ab3a17207af6ce
jguidoux <jeremie.guidoux@tocea.com>
2018-06-04 19:47:02 +0200

    solution module1 -> code challenge

--- module-1-cleancode/m1-code-challenge/src/main/java/com/byoskill/trainings/cleancode/CodeChallenge.java
+++ module-1-cleancode/m1-code-challenge/src/main/java/com/byoskill/trainings/cleancode/CodeChallenge.java
@@ -11,38 +11,18 @@
 
 package com.byoskill.trainings.cleancode;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class CodeChallenge {
 
-    /**
-     * Treat string.
-     *
-     * @param string
-     *            the string
-     * @return the string
-     */
-    public static String treatString(final String string) {
 
-	final int length = string.length();
-	final List<Character> charList = new ArrayList<Character>(string.length());
-	for (int zIndex = -1; zIndex < length - 1; ++zIndex) {
-	    charList.add(string.charAt(zIndex + 1));
-	}
-	final List<Character> charList2 = new LinkedList<Character>();
-	int z2Index = -2;
-	final int res = 8 % 3;
-	while (z2Index < length - 2) {
-	    charList2.add(charList2.size(), charList.get(z2Index + res));
-	    z2Index++;
-	}
-	final char[] array = new char[length];
-	for (int i = 0; i < length; ++i) {
-	    array[length - i - 1] = charList2.get(i);
-	}
-	return new String(array);
+    public static String reverseString(final String contentToReverse ) {
+
+       return StringUtils.reverse(contentToReverse);
 
     }
 }
