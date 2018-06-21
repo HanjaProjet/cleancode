771a8b5a0a2884113c806525f139a386fb1327b9
jguidoux <jeremie.guidoux@tocea.com>
2018-06-21 19:23:59 +0200

    soluction module4 : DIP :

---
+++ module-4-solid/dependency_inversion/src/main/java/com/byoskill/trainings/cleancode/MobileDevice.java
@@ -0,0 +1,11 @@
+package com.byoskill.trainings.cleancode;
+
+/**
+ * Created by mrk on 4/8/14.
+ */
+public class MobileDevice implements Notifier {
+    public void alertWeatherConditions(String weatherConditions) {
+        if (weatherConditions == "rainy")
+            System.out.print("It is rainy");
+    }
+}
---
+++ module-4-solid/dependency_inversion/src/main/java/com/byoskill/trainings/cleancode/Notifier.java
@@ -0,0 +1,8 @@
+package com.byoskill.trainings.cleancode;
+
+/**
+ * Created by mrk on 4/8/14.
+ */
+interface Notifier {
+    public void alertWeatherConditions(String weatherConditions);
+}
